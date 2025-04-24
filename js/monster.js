656029426c9946060edca633560bcc817ed32338
mirih2o <mirih2o@users.noreply.github.com>
2025-04-24 13:12:14 +0200

    Add enemies' behaviour

--- js/monster.js
+++ js/monster.js
@@ -5,7 +5,16 @@ class Monster{
         this.hp = hp;
 	}
 
+    heal(damage){
+        this.hp = Math.min(maxHp, this.hp+damage);
+    }
+
     update(){
+        if(this.stunned){
+            this.stunned = false;
+            return;
+        }
+
         this.doStuff();
     }
 
@@ -23,16 +32,46 @@ class Monster{
 
 	draw(){
         drawSprite(this.sprite, this.tile.x, this.tile.y);
+        this.drawHp();
 	}
+    
+    drawHp(){
+        for(let i=0; i<this.hp; i++){
+            drawSprite(
+                12,
+                this.tile.x + (i%3)*(3/16),
+                this.tile.y - Math.floor(i/3)*(3/16)
+            );
+        }
+    }	
 
     tryMove(dx, dy){
         let newTile = this.tile.getNeighbor(dx,dy);
         if(newTile.passable){
             if(!newTile.monster){
                 this.move(newTile);
+            }else{
+                if(this.isPlayer != newTile.monster.isPlayer){
+                    this.attackedThisTurn = true;
+                    newTile.monster.stunned = true;
+                    newTile.monster.hit(1);
+                }
             }
             return true;
         }
+    }
+
+    hit(damage){
+        this.hp -= damage;
+        if(this.hp <= 0){
+            this.die();
+        }
+    }
+
+    die(){
+        this.dead = true;
+        this.tile.monster = null;
+        this.sprite = 1;
     }
 
     move(tile){
@@ -68,11 +107,28 @@ class Snake extends Monster{
     constructor(tile){
         super(tile, 8, 1);
     }
+
+    doStuff(){
+        this.attackedThisTurn = false;
+        super.doStuff();
+
+        if(!this.attackedThisTurn){
+            super.doStuff();
+        }
+    }
 }
 
 class Tank extends Monster{
     constructor(tile){
         super(tile, 9, 2);
+    }
+
+    update(){
+        let startedStunned = this.stunned;
+        super.update();
+        if(!startedStunned){
+            this.stunned = true;
+        }
     }
 }
 
@@ -80,10 +136,27 @@ class Eater extends Monster{
     constructor(tile){
         super(tile, 10, 1);
     }
+
+    doStuff(){
+        let neighbors = this.tile.getAdjacentNeighbors().filter(t => !t.passable && inBounds(t.x,t.y));
+        if(neighbors.length){
+            neighbors[0].replace(Floor);
+            this.heal(0.5);
+        }else{
+            super.doStuff();
+        }
+    }
 }
 
 class Jester extends Monster{
     constructor(tile){
         super(tile, 11, 2);
+    }
+
+    doStuff(){
+        let neighbors = this.tile.getAdjacentPassableNeighbors();
+        if(neighbors.length){
+            this.tryMove(neighbors[0].x - this.tile.x, neighbors[0].y - this.tile.y);
+        }
     }
 }
